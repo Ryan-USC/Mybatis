abc41f71a03c202501ab3a5d2cb9a4a6e678ec39
Ryan-USC <ylu33178@usc.edu>
2022-02-17 11:50:26 -0800

    add utils

--- ch1-hello-mybatis/src/main/java/org/example/MyApp.java
+++ ch1-hello-mybatis/src/main/java/org/example/MyApp.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.example.domain.Student;
+import org.example.utils.MybatisUtils;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -13,19 +14,19 @@ import java.util.List;
 
 public class MyApp {
     public static void main(String[] args) throws IOException {
-        //访问mybatis读取student数据
-        //1.定义mybatis主配置文件的名称，从类路径的根开始（target/classes）
-        String config = "mybatis.xml";
-        //2.读取这个config表示的文件
-        InputStream in = Resources.getResourceAsStream(config);
-        //3.创建了sqlSessionFactoryBuilder对象
-        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
-        //4.创建sqlSessionFactory对象
-        SqlSessionFactory factory = builder.build(in);
+//        //访问mybatis读取student数据
+//        //1.定义mybatis主配置文件的名称，从类路径的根开始（target/classes）
+//        String config = "mybatis.xml";
+//        //2.读取这个config表示的文件
+//        InputStream in = Resources.getResourceAsStream(config);
+//        //3.创建了sqlSessionFactoryBuilder对象
+//        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+//        //4.创建sqlSessionFactory对象
+//        SqlSessionFactory factory = builder.build(in);
         //5.【****】获取sqlSession对象，从sqlSessionFactory中获取sqlSession
-        SqlSession sqlSession = factory.openSession();
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
         //6.【****】指定要执行的sql语句的标识。sql映射文件中的namespace + "." + 标签的id值
-        String sqlId = "org.example.dao.StudentDao" + "." + "selectStudents";
+        String sqlId = "org.example.dao.StudentDao.selectStudents";
         //7.执行sql语句，通过sqlId找到语句
         List<Student> studentList = sqlSession.selectList(sqlId);
         //8.输出结果
